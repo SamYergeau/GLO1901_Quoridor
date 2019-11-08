1cb940a30d943d1b5fcf90ddd30a612a6ac918a8
Yergeau Samuel <s.yergeau.72@gmail.com>
2019-11-08 16:02:05 -0500

    Added debuter_partie() func

--- api.py
+++ api.py
@@ -27,3 +27,33 @@ def lister_parties(idul):
     else:
         print(f"Le GET sur {url_base+'lister'} a produit le code d'erreur {rep.status_code}.")
     return rep
+
+
+'''
+def débuter_partie(idul)
+Desctiption:
+    Une fonction qui permet de contacter le serveur afin de débuter une partie
+Input:
+    idul (str)
+        L'identifiant IDUL du joueur
+Return:
+    rep (dict)
+        Un dictionnaire contenant:
+            - 'id' (str):
+                L'identifiant unique de la partie
+            - 'état' (dict):
+                Un dictionnaire contenant l'état initial de la partie
+Note:
+    - Si le serveur retourne un message, la fonction soulève une exception RuntimeError suivi de ce message.
+'''
+def débuter_partie(idul):
+    rep = requests.post(url_base+'débuter/', data={'idul': idul})
+    if rep.status_code == 200:
+        # la requête s'est déroulée normalement; décoder le JSON
+        rep = rep.json()
+        # tester pour la présence d'un message dans la réponse
+        if "message" in rep:
+            raise RuntimeError(rep["message"])
+    else:
+        print(f"Le GET sur {url_base+'lister'} a produit le code d'erreur {rep.status_code}.")
+    return rep
