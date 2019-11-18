6124c98d73f37e590031510d62a89c747bc2224e
Samuel Yergeau <samuel.yergeau.2@ulaval.ca>
2019-11-18 13:25:41 -0500

    Added quoridor.py

---
+++ quoridor.py
@@ -0,0 +1,106 @@
+""" Quoridor.py
+Module qui enferme les classes d'encapsulation
+de la structure du jeu
+contient les classes:
+    - Quoridor
+    - QuoridorError(Exception)
+"""
+import networkx as nx
+
+
+def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
+    """
+    Crée le graphe des déplacements admissibles pour les joueurs.
+
+    :param joueurs: une liste des positions (x,y) des joueurs.
+    :param murs_horizontaux: une liste des positions (x,y) des murs horizontaux.
+    :param murs_verticaux: une liste des positions (x,y) des murs verticaux.
+    :returns: le graphe bidirectionnel (en networkX) des déplacements admissibles.
+    """
+    graphe = nx.DiGraph()
+
+    # pour chaque colonne du damier
+    for x in range(1, 10):
+        # pour chaque ligne du damier
+        for y in range(1, 10):
+            # ajouter les arcs de tous les déplacements possibles pour cette tuile
+            if x > 1:
+                graphe.add_edge((x, y), (x-1, y))
+            if x < 9:
+                graphe.add_edge((x, y), (x+1, y))
+            if y > 1:
+                graphe.add_edge((x, y), (x, y-1))
+            if y < 9:
+                graphe.add_edge((x, y), (x, y+1))
+
+    # retirer tous les arcs qui croisent les murs horizontaux
+    for x, y in murs_horizontaux:
+        graphe.remove_edge((x, y-1), (x, y))
+        graphe.remove_edge((x, y), (x, y-1))
+        graphe.remove_edge((x+1, y-1), (x+1, y))
+        graphe.remove_edge((x+1, y), (x+1, y-1))
+
+    # retirer tous les arcs qui croisent les murs verticaux
+    for x, y in murs_verticaux:
+        graphe.remove_edge((x-1, y), (x, y))
+        graphe.remove_edge((x, y), (x-1, y))
+        graphe.remove_edge((x-1, y+1), (x, y+1))
+        graphe.remove_edge((x, y+1), (x-1, y+1))
+
+    # retirer tous les arcs qui pointent vers les positions des joueurs
+    # et ajouter les sauts en ligne droite ou en diagonale, selon le cas
+    for joueur in map(tuple, joueurs):
+
+        for prédécesseur in list(graphe.predecessors(joueur)):
+            graphe.remove_edge(prédécesseur, joueur)
+
+            # si admissible, ajouter un lien sauteur
+            successeur = (2*joueur[0]-prédécesseur[0], 2*joueur[1]-prédécesseur[1])
+
+            if successeur in graphe.successors(joueur) and successeur not in joueurs:
+                # ajouter un saut en ligne droite
+                graphe.add_edge(prédécesseur, successeur)
+
+            else:
+                # ajouter les liens en diagonal
+                for successeur in list(graphe.successors(joueur)):
+                    if prédécesseur != successeur and successeur not in joueurs:
+                        graphe.add_edge(prédécesseur, successeur)
+
+    # ajouter les noeuds objectifs des deux joueurs
+    for x in range(1, 10):
+        graphe.add_edge((x, 9), 'B1')
+        graphe.add_edge((x, 1), 'B2')
+
+    return graphe
+
+
+class QuoridorError(Exception):
+    """QuoridorError    
+    Classe pour gérer les exceptions survenue dans la classe Quoridor    
+    Arguments:
+        Exception {[type]} -- [description]
+    """
+    print("quoridorerror")
+
+
+class Quoridor:
+
+    def __init__(self, joueurs, murs=None):
+        """
+        __init__
+        Initialisation de la classe Quoridor
+        Arguments:
+            joueurs {list}
+                -- Une liste de 2 joueurs. chaque joueur est: (dict) ou (str)
+                    - (str): nom du joueur
+                    - (dict)
+                        + 'nom' (str)   -- Nom du joueur
+                        + 'murs' (int)  -- Nombre de murs que le joueur peut encore placer
+                        + 'pos' (tuple) -- position (x, y) du joueur
+        Keyword Arguments:
+            murs {dict} (default: {None})
+            -- 'horzontaux': [list of tuples]
+                Une liste de tuples (x, y) représentant la position des différents
+                murs horizontaux dans la partie
+        """
